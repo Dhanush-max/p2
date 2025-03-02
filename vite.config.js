53cd2546c0298c09e460021ea4b66f002910248b
Dhanush-max <dhanushkumarm003@gmail.com>
2025-03-02 14:39:47 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/p2/",
+  
   css: {
     postcss: {
       plugins: [(await import("autoprefixer")).default],
