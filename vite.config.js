8062f61b7f05cdc07aa1bc904e0114a3eec93c18
sharankumarreddyk <167206944+sharankumarreddyk@users.noreply.github.com>
2025-04-12 13:12:56 +0530

    Fix:Hosting

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/Dhanush-max/p2/",
   css: {
     postcss: {
       plugins: [(await import("autoprefixer")).default],
